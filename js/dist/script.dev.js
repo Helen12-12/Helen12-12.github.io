903071b345665ad0e55a59ceedba3ce995f018ae
Helen12-12 <77459662+Helen12-12@users.noreply.github.com>
2021-04-02 15:33:14 +0300

    Add files via upload

---
+++ js/dist/script.dev.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var hamburger = document.querySelector('.hamburger'),
+    menu = document.querySelector('.menu'),
+    closeElem = document.querySelector('.menu__close');
+hamburger.addEventListener('click', function () {
+  menu.classList.add('active');
+});
+closeElem.addEventListener('click', function () {
+  menu.classList.remove('active');
+});
